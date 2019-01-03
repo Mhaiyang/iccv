abfac8084862b38f3ad3ec1d7d64bae058494812
taylor <mhy845879017@gmail.com>
2019-01-04 02:29:42 +0800

    TaylorMei taylor 2018.1.3

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+# msd_training_root = "/home/taylor/data/MSD9/train"
+# msd_testing_root = "/home/taylor/data/MSD9/test"
+# backbone_path = "/home/taylor/iccv/backbone/resnet101.pth"
+
 # msd_training_root = "/home/iccd/data/MSD9/train"
 # msd_testing_root = "/home/iccd/data/MSD9/test"
 # backbone_path = "/home/iccd/iccv/backbone/resnet101.pth"
 
-msd_training_root = "/home/taylor/data/MSD9/train"
-msd_testing_root = "/home/taylor/data/MSD9/test"
-backbone_path = "/home/taylor/iccv/backbone/resnet101.pth"
+msd_training_root = "/root/data/train"
+msd_testing_root = "/root//data/test"
+backbone_path = "/root/backbone/resnet101.pth"

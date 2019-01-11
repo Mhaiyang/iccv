cbcfc41508cf551a6532d58b97adfb4e99c7aa1e
Mhaiyang <845879017@qq.com>
2019-01-11 16:38:24 +0800

    Taylor iccd 2018.1.10

--- train_base2.py
+++ train_base2.py
@@ -40,7 +40,7 @@ exp_name = 'BASE2'
 # batch size of 8 with resolution of 416*416 is exactly OK for the GTX 1080Ti GPU
 args = {
     'epoch_num': 100,
-    'train_batch_size': 8,
+    'train_batch_size': 6,
     'val_batch_size': 8,
     'last_epoch': 0,
     'lr': 1e-3,

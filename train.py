730545fd28b6bdd9376ef70bee5d7d40de27c5d0
taylor <mhy845879017@gmail.com>
2019-01-12 04:15:18 +0800

    TaylorMei taylor 2019.1.11

--- train.py
+++ train.py
@@ -9,37 +9,38 @@ from torch.backends import cudnn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from tensorboardX import SummaryWriter
+from tqdm import tqdm
 
 import joint_transforms
 from config import msd_training_root, msd_testing_root
 from config import backbone_path
 from dataset import ImageFolder
 from misc import AvgMeter, check_mkdir
-from model.fuse import FUSE
+from model.edge_cbam_x_ccl import EDGE_CBAM_X_CCL
 
 cudnn.benchmark = True
 
-device_ids = [0]
+device_ids = [1]
 # device_ids = [2, 3, 4, 5]
 # device_ids = [0, 1]
 
 ckpt_path = './ckpt'
-exp_name = 'DSC'
+exp_name = 'EDGE_CBAM_X_CCL'
 
 # batch size of 8 with resolution of 416*416 is exactly OK for the GTX 1080Ti GPU
 args = {
-    'iter_num': 12000,
-    'train_batch_size': 8,
+    'epoch_num': 100,
+    'train_batch_size': 6,
     'val_batch_size': 8,
     'last_iter': 0,
-    'lr': 1e-8,
+    'lr': 1e-4,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.9,
     'snapshot': '',
-    'scale': 416,
-    'add_graph': False,
-    'poly_train': False
+    'scale': 512,
+    'add_graph': True,
+    'poly_train': True
 }
 
 # Path.
@@ -80,7 +81,7 @@ bce_logit = nn.BCEWithLogitsLoss().cuda(device_ids[0])
 def main():
     print(args)
 
-    net = DSC().cuda(device_ids[0]).train()
+    net = EDGE_CBAM_X_CCL().cuda(device_ids[0]).train()
     if args['add_graph']:
         writer.add_graph(net, input_to_model=torch.rand(
             args['train_batch_size'], 3, args['scale'], args['scale']).cuda(device_ids[0]))
@@ -90,7 +91,7 @@ def main():
         {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
          'lr': 2 * args['lr']},
         {'params': [param for name, param in net.named_parameters() if name[-4:] != 'bias'],
-         'lr': args['lr'], 'weight_decay': args['weight_decay']}
+         'lr': 1 * args['lr'], 'weight_decay': args['weight_decay']}
     ], momentum=args['momentum'])
 
     if len(args['snapshot']) > 0:
@@ -106,116 +107,99 @@ def main():
 
 
 def train(net, optimizer):
-    curr_iter = args['last_iter']
-    while True:
-        train_loss_record, \
-        loss_f3_record, loss_f2_record, loss_f1_record, loss_f0_record, \
-        loss_b3_record, loss_b2_record, loss_b1_record, loss_b0_record, \
-        loss_fb_record, loss_e_record = AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), \
-                         AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter()
+    curr_iter = 1
 
-        for i, data in enumerate(train_loader):
+    for epoch in range(args['last_epoch'] + 1, args['last_epoch'] + 1 + args['epoch_num']):
+        loss_f4_record, loss_f3_record, loss_f2_record, loss_f1_record, \
+        loss_b4_record, loss_b3_record, loss_b2_record, loss_b1_record, \
+        loss_e_record, loss_fb_record, loss_record = AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), \
+                                                     AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), \
+                                                     AvgMeter(), AvgMeter(), AvgMeter()
+
+        train_iterator = tqdm(train_loader, total=len(train_loader))
+        for data in train_iterator:
             if args['poly_train']:
-                optimizer.param_groups[0]['lr'] = 2 * args['lr'] * (1 - float(curr_iter) / args['iter_num']
-                                                                    ) ** args['lr_decay']
-                optimizer.param_groups[1]['lr'] = args['lr'] * (1 - float(curr_iter) / args['iter_num']
-                                                                ) ** args['lr_decay']
+                base_lr = args['lr'] * (1 - float(curr_iter) / (args['epoch_num'] * len(train_loader))) ** args['lr_decay']
+                optimizer.param_groups[0]['lr'] = 2 * base_lr
+                optimizer.param_groups[1]['lr'] = 1 * base_lr
 
             inputs, labels, edges = data
             batch_size = inputs.size(0)
             inputs = Variable(inputs).cuda(device_ids[0])
             labels = Variable(labels).cuda(device_ids[0])
-            edges = Variable(edges).cuda(device_ids[0])
 
             optimizer.zero_grad()
 
-            predict_f3, predict_f2, predict_f1, predict_f0, \
-            predict_b3, predict_b2, predict_b1, predict_b0, predict_e, predict_fb = net(inputs)
+            predict_f4, predict_f3, predict_f2, predict_f1, \
+            predict_b4, predict_b3, predict_b2, predict_b1, predict_e, predict_fb = net(inputs)
 
+            loss_f4 = bce_logit(predict_f4, labels)
             loss_f3 = bce_logit(predict_f3, labels)
             loss_f2 = bce_logit(predict_f2, labels)
             loss_f1 = bce_logit(predict_f1, labels)
-            loss_f0 = bce_logit(predict_f0, labels)
 
+            loss_b4 = bce(1 - torch.sigmoid(predict_b4), labels)
             loss_b3 = bce(1 - torch.sigmoid(predict_b3), labels)
             loss_b2 = bce(1 - torch.sigmoid(predict_b2), labels)
             loss_b1 = bce(1 - torch.sigmoid(predict_b1), labels)
-            loss_b0 = bce(1 - torch.sigmoid(predict_b0), labels)
 
             loss_e = bce_logit(predict_e, edges)
 
             loss_fb = bce_logit(predict_fb, labels)
 
-            loss = loss_f3 + loss_f2 + loss_f1 + loss_f0 + \
-                   loss_b3 + loss_b2 + loss_b1 + loss_b0 + 10*loss_e + 8*loss_fb
+            loss = loss_f4 + loss_f3 + loss_f2 + loss_f1 + \
+                   loss_b4 + loss_b3 + loss_b2 + loss_b1 + 10 * loss_e + 8 * loss_fb
 
             loss.backward()
 
             optimizer.step()
 
-            train_loss_record.update(loss.data, batch_size)
+            loss_record.update(loss.data, batch_size)
+            loss_f4_record.update(loss_f4.data, batch_size)
             loss_f3_record.update(loss_f3.data, batch_size)
             loss_f2_record.update(loss_f2.data, batch_size)
             loss_f1_record.update(loss_f1.data, batch_size)
-            loss_f0_record.update(loss_f0.data, batch_size)
+            loss_b4_record.update(loss_b4.data, batch_size)
             loss_b3_record.update(loss_b3.data, batch_size)
             loss_b2_record.update(loss_b2.data, batch_size)
             loss_b1_record.update(loss_b1.data, batch_size)
-            loss_b0_record.update(loss_b0.data, batch_size)
             loss_e_record.update(loss_e.data, batch_size)
             loss_fb_record.update(loss_fb.data, batch_size)
 
-            if curr_iter % 10 == 0:
+            if curr_iter % 50 == 0:
                 writer.add_scalar('Total loss', loss, curr_iter)
+                writer.add_scalar('f4 loss', loss_f4, curr_iter)
                 writer.add_scalar('f3 loss', loss_f3, curr_iter)
                 writer.add_scalar('f2 loss', loss_f2, curr_iter)
                 writer.add_scalar('f1 loss', loss_f1, curr_iter)
-                writer.add_scalar('f0 loss', loss_f0, curr_iter)
+                writer.add_scalar('b4 loss', loss_b4, curr_iter)
                 writer.add_scalar('b3 loss', loss_b3, curr_iter)
                 writer.add_scalar('b2 loss', loss_b2, curr_iter)
                 writer.add_scalar('b1 loss', loss_b1, curr_iter)
-                writer.add_scalar('b0 loss', loss_b0, curr_iter)
                 writer.add_scalar('e loss', loss_e, curr_iter)
                 writer.add_scalar('fb loss', loss_fb, curr_iter)
 
-            curr_iter += 1
-
-            log = '[iter %d], [sum %.5f],  [f3 %.5f], [f2 %.5f], [f1 %.5f], [f0 %.5f] ' \
-                  '[b3 %.5f], [b2 %.5f], [b1 %.5f], [b0 %.5f], [e %.5f], [fb %.5f], [lr %.13f]' % \
-                  (curr_iter, train_loss_record.avg,
-                   loss_f3_record.avg, loss_f2_record.avg, loss_f1_record.avg, loss_f0_record.avg,
-                   loss_b3_record.avg, loss_b2_record.avg, loss_b1_record.avg, loss_b0_record.avg,
-                   loss_e_record.avg, loss_fb_record.avg, optimizer.param_groups[1]['lr'])
-            print(log)
+            log = '[iter %d], [sum %.5f],  [f4 %.5f], [f3 %.5f], [f2 %.5f], [f1 %.5f] ' \
+                  '[b4 %.5f], [b3 %.5f], [b2 %.5f], [b1 %.5f], [e %.5f], [fb %.5f], [lr %.5f]' % \
+                  (curr_iter, loss_record.avg,
+                   loss_f4_record.avg, loss_f3_record.avg, loss_f2_record.avg, loss_f1_record.avg,
+                   loss_b4_record.avg, loss_b3_record.avg, loss_b2_record.avg, loss_b1_record.avg,
+                   loss_e_record.avg, loss_fb_record.avg, base_lr)
+            train_iterator.set_description(log)
             open(log_path, 'a').write(log + '\n')
 
-            # if curr_iter % 5 == 0:
-            #     net.eval()
-            #     validate(net, curr_iter, 128)
-            #     net.train()
+            curr_iter += 1
 
-            if curr_iter >= args['iter_num']:
-                net.cpu()
-                torch.save(net.module.state_dict(), os.path.join(ckpt_path, exp_name, '%d.pth' % curr_iter))
-                # torch.save(optimizer.state_dict(), os.path.join(ckpt_path, exp_name, '%d_optim.pth' % curr_iter))
-                print("Optimization Have Done!")
-                return
+        if epoch in args['save_point']:
+            net.cpu()
+            torch.save(net.module.state_dict(), os.path.join(ckpt_path, exp_name, '%d.pth' % epoch))
+            net.cuda(device_ids[0])
 
-
-def validate(net, curr_iter, total_iter):
-    loss_record = AvgMeter()
-    for x in range(total_iter):
-        for y, data in enumerate(val_loader):
-            inputs, labels, _ = data
-            batch_size = inputs.size(0)
-            inputs = Variable(inputs).cuda(device_ids[0])
-            labels = Variable(labels).cuda(device_ids[0])
-
-            _, predict = net(inputs)
-            loss = bce(predict, labels)
-            loss_record.update(loss.data, batch_size)
-    print("Validation Loss: {}".format(loss_record.avg))
-    writer.add_scalar('val loss', loss_record.avg, curr_iter)
+        if epoch >= args['epoch_num']:
+            net.cpu()
+            torch.save(net.module.state_dict(), os.path.join(ckpt_path, exp_name, '%d.pth' % epoch))
+            print("Optimization Have Done!")
+            return
 
 
 if __name__ == '__main__':

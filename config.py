02adc4fbf9a4e610641f097647ebe213063bdf71
taylor <mhy845879017@gmail.com>
2019-01-13 05:45:17 +0800

    TaylorMei taylor 2019.1.12

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 # backbone_path = "/home/taylor/iccv/backbone/resnet101.pth"
 # backbone_path = '/home/taylor/iccv/backbone/resnext/resnext_101_32x4d.pth'
 
-msd_training_root = "/home/iccd/data/MSD9/train"
-msd_testing_root = "/home/iccd/data/MSD9/test"
+# msd_training_root = "/home/iccd/data/MSD9/train"
+# msd_testing_root = "/home/iccd/data/MSD9/test"
 # backbone_path = "/home/iccd/iccv/backbone/resnet101.pth"
-backbone_path = '/home/iccd/iccv/backbone/resnext/resnext_101_32x4d.pth'
+# backbone_path = '/home/iccd/iccv/backbone/resnext/resnext_101_32x4d.pth'
 
-# msd_training_root = "/root/data/train"
-# msd_testing_root = "/root//data/test"
+msd_training_root = "/root/data/train"
+msd_testing_root = "/root//data/test"
 # backbone_path = "/root/backbone/resnet101.pth"
-# backbone_path = '/root/backbone/resnext/resnext_101_32x4d.pth'
+backbone_path = '/root/backbone/resnext/resnext_101_32x4d.pth'

2bf3adbb4da84e6f48f141c55179d95ee7317b91
Mhaiyang <845879017@qq.com>
2019-01-14 21:44:50 +0800

    Taylor iccd 2018.1.14

--- train_base23.py
+++ train_base23.py
@@ -95,7 +95,7 @@ class WL(nn.Module):
         pred = torch.sigmoid(pred_logit)
         batch_size =truth.size(0)
 
-        truth_flat = truth.contiguous.view(batch_size, -1)
+        truth_flat = truth.contiguous().view(batch_size, -1)
 
         N_p = truth_flat.sum(1)
         N_p = torch.where(N_p < self.threshold, torch.tensor(self.threshold), N_p)
@@ -107,7 +107,7 @@ class WL(nn.Module):
 
         L1 = nn.BCELoss(w1)(pred, truth)
 
-        pred_flat = pred.contiguous.view(batch_size, -1)
+        pred_flat = pred.contiguous().view(batch_size, -1)
         pred_flat_bool = torch.where(pred_flat >= 0.5, torch.tensor(1.), torch.tensor(0))
         TP = (pred_flat_bool * truth_flat).sum(1)
         TN = ((1 - pred_flat_bool) * (1 - truth_flat)).sum(1)

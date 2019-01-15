b6e3348f7bdea0577224216d0abe0cb84dd387ac
Mhaiyang <845879017@qq.com>
2019-01-15 22:30:55 +0800

    Taylor iccd 2018.1.15

--- train_base234.py
+++ train_base234.py
@@ -45,7 +45,7 @@ args = {
     'train_batch_size': 8,
     'val_batch_size': 8,
     'last_epoch': 0,
-    'lr': 1e-3,
+    'lr': 1e-2,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.9,

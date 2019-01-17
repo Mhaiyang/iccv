fa6db7f79dca6a8e8c44fc4250d9aae0e9ec0834
Mhaiyang <845879017@qq.com>
2019-01-17 21:38:46 +0800

    Taylor iccd 2018.1.17

--- train_base5.py
+++ train_base5.py
@@ -33,8 +33,8 @@ import loss as L
 cudnn.benchmark = True
 
 # device_ids = [0]
-# device_ids = [4, 5]
-device_ids = [1, 0]
+device_ids = [4, 5]
+# device_ids = [1, 0]
 
 ckpt_path = './ckpt'
 exp_name = 'BASE5'
@@ -80,7 +80,7 @@ target_transform = transforms.ToTensor()
 # Prepare Data Set.
 train_set = ImageFolder(msd_training_root, joint_transform, img_transform, target_transform)
 print("Train set: {}".format(train_set.__len__()))
-train_loader = DataLoader(train_set, batch_size=args['train_batch_size'], num_workers=64, shuffle=True)
+train_loader = DataLoader(train_set, batch_size=args['train_batch_size'], num_workers=0, shuffle=True)
 val_set = ImageFolder(msd_testing_root, val_joint_transform, img_transform, target_transform)
 print("Validation Set: {}".format(val_set.__len__()))
 val_loader = DataLoader(val_set, batch_size=args['val_batch_size'], num_workers=8, shuffle=False)

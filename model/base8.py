fd38ed9eeddeebfda7aaf9c0ae7fca231107138b
taylor <mhy845879017@gmail.com>
2019-01-20 00:24:55 +0800

    TaylorMei taylor 2019.1.19

---
+++ model/base8.py
@@ -0,0 +1,360 @@
+"""
+  @Time    : 2019-1-19 23:21
+  @Author  : TaylorMei
+  @Email   : mhy845879017@gmail.com
+  
+  @Project : iccv
+  @File    : base8.py
+  @Function: 
+  
+"""
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from backbone.resnext.resnext101_regular import ResNeXt101
+
+
+###################################################################
+# ########################## CBAM #################################
+###################################################################
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True,
+                 bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+        )
+        self.pool_types = pool_types
+
+    def forward(self, x):
+        channel_att_sum = True
+        for pool_type in self.pool_types:
+            if pool_type == 'avg':
+                avg_pool = F.avg_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(avg_pool)
+            elif pool_type == 'max':
+                max_pool = F.max_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(max_pool)
+            elif pool_type == 'lp':
+                lp_pool = F.lp_pool2d(x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(lp_pool)
+            elif pool_type == 'lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp(lse_pool)
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = F.sigmoid(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
+
+
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = F.sigmoid(x_out)  # broadcasting
+        return x * scale
+
+
+class CBAM(nn.Module):
+    def __init__(self, gate_channels=128, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False):
+        super(CBAM, self).__init__()
+        self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
+
+
+###################################################################
+# ########################## CCL ##################################
+###################################################################
+class CCL(nn.Module):
+    def __init__(self, in_planes, out_planes, rate):
+        super(CCL, self).__init__()
+        self.input_conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0)
+        self.output_conv = nn.Conv2d(out_planes * 4, out_planes, kernel_size=1, stride=1, padding=0)
+        self.local = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1)
+        self.context = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=rate, dilation=rate)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.input_conv(x)
+        local_1 = self.local(x)
+        context_1 = self.context(x)
+        ccl_1 = local_1 - context_1
+        ccl_1 = self.bn(ccl_1)
+        ccl_1 = self.relu(ccl_1)
+
+        local_2 = self.local(ccl_1)
+        context_2 = self.context(ccl_1)
+        ccl_2 = local_2 - context_2
+        ccl_2 = self.bn(ccl_2)
+        ccl_2 = self.relu(ccl_2)
+
+        local_3 = self.local(ccl_2)
+        context_3 = self.context(ccl_2)
+        ccl_3 = local_3 - context_3
+        ccl_3 = self.bn(ccl_3)
+        ccl_3 = self.relu(ccl_3)
+
+        local_4 = self.local(ccl_3)
+        context_4 = self.context(ccl_3)
+        ccl_4 = local_4 - context_4
+        ccl_4 = self.bn(ccl_4)
+        ccl_4 = self.relu(ccl_4)
+
+        ccl_fusion = torch.cat((ccl_1, ccl_2, ccl_3, ccl_4), 1)
+        output = self.output_conv(ccl_fusion)
+
+        return output
+
+
+###################################################################
+# ########################## MHY ##################################
+###################################################################
+
+
+###################################################################
+# ########################## NETWORK ##############################
+###################################################################
+class BASE8(nn.Module):
+    def __init__(self, backbone_path=None):
+        super(BASE8, self).__init__()
+        resnext = ResNeXt101(backbone_path)
+        self.layer0 = resnext.layer0
+        self.layer1 = resnext.layer1
+        self.layer2 = resnext.layer2
+        self.layer3 = resnext.layer3
+        self.layer4 = resnext.layer4
+
+        # Mirror Region Stream
+        self.f_ccl_4 = CCL(2048, 512, 2)
+        self.f_ccl_3 = CCL(1024, 256, 3)
+        self.f_ccl_2 = CCL(512, 128, 4)
+        self.f_ccl_1 = CCL(256, 64, 5)
+
+        self.f_sc_4 = nn.Conv2d(2048, 512, 1)
+        self.f_sc_3 = nn.Conv2d(2048, 512, 1)
+        self.f_sc_2 = nn.Conv2d(2048, 512, 1)
+        self.f_sc_1 = nn.Conv2d(2048, 512, 1)
+
+        self.f_fusion_4 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.f_fusion_3 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.f_fusion_2 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.f_fusion_1 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+
+        self.f_up_4 = nn.Sequential(nn.ConvTranspose2d(512, 64, 16, 8, 4), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_up_3 = nn.Sequential(nn.ConvTranspose2d(256, 64, 8, 4, 2), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_up_2 = nn.Sequential(nn.ConvTranspose2d(128, 64, 4, 2, 1), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_up_1 = nn.Sequential(nn.Conv2d(64, 64, 1, 1, 1), nn.BatchNorm2d(64), nn.ReLU())
+
+        self.f_cbam_4 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_cbam_3 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_cbam_2 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_cbam_1 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.f_cbam_concat = nn.Sequential(CBAM(256), nn.BatchNorm2d(256), nn.ReLU())
+
+        self.f_predict_4 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.f_predict_3 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.f_predict_2 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.f_predict_1 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.f_predict_concat = nn.Conv2d(256, 1, 3, 1, 1)
+
+        # Non-Mirror Region Stream
+        self.b_ccl_4 = CCL(2048, 512, 2)
+        self.b_ccl_3 = CCL(1024, 256, 3)
+        self.b_ccl_2 = CCL(512, 128, 4)
+        self.b_ccl_1 = CCL(256, 64, 5)
+
+        self.b_sc_4 = nn.Conv2d(2048, 512, 1)
+        self.b_sc_3 = nn.Conv2d(2048, 512, 1)
+        self.b_sc_2 = nn.Conv2d(2048, 512, 1)
+        self.b_sc_1 = nn.Conv2d(2048, 512, 1)
+
+        self.b_fusion_4 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.b_fusion_3 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.b_fusion_2 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+        self.b_fusion_1 = nn.Sequential(nn.Conv2d(1024, 512, 1), nn.BatchNorm2d(512), nn.ReLU())
+
+        self.b_up_4 = nn.Sequential(nn.ConvTranspose2d(512, 64, 16, 8, 4), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_up_3 = nn.Sequential(nn.ConvTranspose2d(256, 64, 8, 4, 2), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_up_2 = nn.Sequential(nn.ConvTranspose2d(128, 64, 4, 2, 1), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_up_1 = nn.Sequential(nn.Conv2d(64, 64, 1, 1, 1), nn.BatchNorm2d(64), nn.ReLU())
+
+        self.b_cbam_4 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_cbam_3 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_cbam_2 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_cbam_1 = nn.Sequential(CBAM(64), nn.BatchNorm2d(64), nn.ReLU())
+        self.b_cbam_concat = nn.Sequential(CBAM(256), nn.BatchNorm2d(256), nn.ReLU())
+
+        self.b_predict_4 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.b_predict_3 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.b_predict_2 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.b_predict_1 = nn.Conv2d(64, 1, 3, 1, 1)
+        self.b_predict_concat = nn.Conv2d(256, 1, 3, 1, 1)
+
+        # Feature Mosaic
+        self.output_predict = nn.Sequential(nn.BatchNorm2d(256), nn.ReLU(), nn.Conv2d(256, 1, 3, 1, 1))
+
+        for m in self.modules():
+            if isinstance(m, nn.ReLU):
+                m.inplace = True
+
+    def forward(self, x):
+        layer0 = self.layer0(x)
+        layer1 = self.layer1(layer0)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)
+        layer4 = self.layer4(layer3)
+
+        # Mirror Region Stream
+        f_ccl_4 = self.f_ccl_4(layer4)
+        f_ccl_3 = self.f_ccl_3(layer3)
+        f_ccl_2 = self.f_ccl_2(layer2)
+        f_ccl_1 = self.f_ccl_1(layer1)
+
+        f_sc_4 = self.f_sc_4(layer4)
+        f_sc_3 = self.f_sc_3(layer4)
+        f_sc_2 = self.f_sc_2(layer4)
+        f_sc_1 = self.f_sc_1(layer4)
+
+        f_fusion_4 = self.f_fusion_4(torch.cat((f_sc_4, f_ccl_4), 1))
+        f_fusion_3 = self.f_fusion_3(torch.cat((f_sc_3, f_ccl_3), 1))
+        f_fusion_2 = self.f_fusion_2(torch.cat((f_sc_2, f_ccl_2), 1))
+        f_fusion_1 = self.f_fusion_1(torch.cat((f_sc_1, f_ccl_1), 1))
+
+        f_up_4 = self.f_up_4(f_fusion_4)
+        f_up_3 = self.f_up_3(f_fusion_3)
+        f_up_2 = self.f_up_2(f_fusion_2)
+        f_up_1 = self.f_up_1(f_fusion_1)
+
+        f_cbam_4 = self.f_cbam_4(f_up_4)
+        f_cbam_3 = self.f_cbam_3(f_up_3)
+        f_cbam_2 = self.f_cbam_2(f_up_2)
+        f_cbam_1 = self.f_cbam_1(f_up_1)
+
+        f_predict_4 = self.f_predict_4(f_cbam_4)
+        f_predict_3 = self.f_predict_3(f_cbam_3)
+        f_predict_2 = self.f_predict_2(f_cbam_2)
+        f_predict_1 = self.f_predict_1(f_cbam_1)
+
+        f_cbam_concat = self.f_cbam_concat(torch.cat((f_cbam_1, f_cbam_2, f_cbam_3, f_cbam_4), 1))
+
+        f_predict_concat = self.f_predict_concat(f_cbam_concat)
+
+        # Boundary Stream
+        b_ccl_4 = self.b_ccl_4(layer4)
+        b_ccl_3 = self.b_ccl_3(layer3)
+        b_ccl_2 = self.b_ccl_2(layer2)
+        b_ccl_1 = self.b_ccl_1(layer1)
+
+        b_sc_4 = self.b_sc_4(layer4)
+        b_sc_3 = self.b_sc_3(layer4)
+        b_sc_2 = self.b_sc_2(layer4)
+        b_sc_1 = self.b_sc_1(layer4)
+
+        b_fusion_4 = self.b_fusion_4(torch.cat((b_sc_4, b_ccl_4), 1))
+        b_fusion_3 = self.b_fusion_3(torch.cat((b_sc_3, b_ccl_3), 1))
+        b_fusion_2 = self.b_fusion_2(torch.cat((b_sc_2, b_ccl_2), 1))
+        b_fusion_1 = self.b_fusion_1(torch.cat((b_sc_1, b_ccl_1), 1))
+
+        b_up_4 = self.b_up_4(b_fusion_4)
+        b_up_3 = self.b_up_3(b_fusion_3)
+        b_up_2 = self.b_up_2(b_fusion_2)
+        b_up_1 = self.b_up_1(b_fusion_1)
+
+        b_cbam_4 = self.b_cbam_4(b_up_4)
+        b_cbam_3 = self.b_cbam_3(b_up_3)
+        b_cbam_2 = self.b_cbam_2(b_up_2)
+        b_cbam_1 = self.b_cbam_1(b_up_1)
+
+        b_predict_4 = self.b_predict_4(b_cbam_4)
+        b_predict_3 = self.b_predict_3(b_cbam_3)
+        b_predict_2 = self.b_predict_2(b_cbam_2)
+        b_predict_1 = self.b_predict_1(b_cbam_1)
+
+        b_cbam_concat = self.b_cbam_concat(torch.cat((b_cbam_1, b_cbam_2, b_cbam_3, b_cbam_4), 1))
+
+        b_predict_concat = self.b_predict_concat(b_cbam_concat)
+
+        # Feature Mosaic
+        output_feature = f_cbam_concat.mul(torch.sigmoid(f_predict_concat)).mul(1 - torch.sigmoid(b_predict_concat)) - \
+                         b_cbam_concat.mul(torch.sigmoid(b_predict_concat)).mul(1 - torch.sigmoid(f_predict_concat))
+
+        output_predict = self.output_predict(output_feature)
+
+        # Upsample
+        f_predict_4 = F.interpolate(f_predict_4, size=x.size()[2:], mode='bilinear', align_corners=True)
+        f_predict_3 = F.interpolate(f_predict_3, size=x.size()[2:], mode='bilinear', align_corners=True)
+        f_predict_2 = F.interpolate(f_predict_2, size=x.size()[2:], mode='bilinear', align_corners=True)
+        f_predict_1 = F.interpolate(f_predict_1, size=x.size()[2:], mode='bilinear', align_corners=True)
+        f_predict_concat = F.interpolate(f_predict_concat, size=x.size()[2:], mode='bilinear', align_corners=True)
+        b_predict_4 = F.interpolate(b_predict_4, size=x.size()[2:], mode='bilinear', align_corners=True)
+        b_predict_3 = F.interpolate(b_predict_3, size=x.size()[2:], mode='bilinear', align_corners=True)
+        b_predict_2 = F.interpolate(b_predict_2, size=x.size()[2:], mode='bilinear', align_corners=True)
+        b_predict_1 = F.interpolate(b_predict_1, size=x.size()[2:], mode='bilinear', align_corners=True)
+        b_predict_concat = F.interpolate(b_predict_concat, size=x.size()[2:], mode='bilinear', align_corners=True)
+        output_predict = F.interpolate(output_predict, size=x.size()[2:], mode='bilinear', align_corners=True)
+
+        if self.training:
+            return f_predict_4, f_predict_3, f_predict_2, f_predict_1, f_predict_concat, \
+                   b_predict_4, b_predict_3, b_predict_2, b_predict_1, b_predict_concat, output_predict
+
+        return torch.sigmoid(f_predict_concat), torch.sigmoid(b_predict_concat), torch.sigmoid(output_predict)
---
+++ train_base8.py
@@ -0,0 +1,212 @@
+"""
+  @Time    : 2019-1-19 22:55
+  @Author  : TaylorMei
+  @Email   : mhy845879017@gmail.com
+  
+  @Project : iccv
+  @File    : train_base8.py
+  @Function: 
+  
+"""
+import datetime
+import os
+
+import torch
+from torch import nn
+from torch import optim
+from torch.autograd import Variable
+from torch.backends import cudnn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+
+import joint_transforms
+from config import msd_training_root, msd_testing_root
+from config import backbone_path
+from dataset import ImageFolder
+from misc import AvgMeter, check_mkdir
+from model.base8 import BASE8
+
+import loss as L
+
+cudnn.benchmark = True
+
+# device_ids = [0]
+# device_ids = [4, 5]
+device_ids = [1, 0]
+
+ckpt_path = './ckpt'
+exp_name = 'BASE8'
+
+args = {
+    'epoch_num': 100,
+    'train_batch_size': 6,
+    'val_batch_size': 8,
+    'last_epoch': 0,
+    'lr': 1e-3,
+    'lr_decay': 0.9,
+    'weight_decay': 5e-4,
+    'momentum': 0.9,
+    'snapshot': '',
+    'scale': 512,
+    'save_point': [40, 60, 80],
+    'add_graph': True,
+    'poly_train': True
+}
+
+# Path.
+check_mkdir(ckpt_path)
+check_mkdir(os.path.join(ckpt_path, exp_name))
+vis_path = os.path.join(ckpt_path, exp_name, 'log')
+check_mkdir(vis_path)
+log_path = os.path.join(ckpt_path, exp_name, str(datetime.datetime.now()) + '.txt')
+writer = SummaryWriter(log_dir=vis_path, comment=exp_name)
+
+# Transform Data.
+joint_transform = joint_transforms.Compose([
+    joint_transforms.RandomRotate(),
+    joint_transforms.Resize((args['scale'], args['scale']))
+])
+val_joint_transform = joint_transforms.Compose([
+    joint_transforms.Resize((args['scale'], args['scale']))
+])
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # maybe can optimized.
+])
+target_transform = transforms.ToTensor()
+
+# Prepare Data Set.
+train_set = ImageFolder(msd_training_root, joint_transform, img_transform, target_transform)
+print("Train set: {}".format(train_set.__len__()))
+train_loader = DataLoader(train_set, batch_size=args['train_batch_size'], num_workers=64, shuffle=True)
+val_set = ImageFolder(msd_testing_root, val_joint_transform, img_transform, target_transform)
+print("Validation Set: {}".format(val_set.__len__()))
+val_loader = DataLoader(val_set, batch_size=args['val_batch_size'], num_workers=8, shuffle=False)
+
+
+def main():
+    print(args)
+
+    net = BASE8(backbone_path).cuda(device_ids[0]).train()
+    if args['add_graph']:
+        writer.add_graph(net, input_to_model=torch.rand(
+            args['train_batch_size'], 3, args['scale'], args['scale']).cuda(device_ids[0]))
+
+    optimizer = optim.SGD([
+        {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
+         'lr': 2 * args['lr']},
+        {'params': [param for name, param in net.named_parameters() if name[-4:] != 'bias'],
+         'lr': 1 * args['lr'], 'weight_decay': args['weight_decay']}
+    ], momentum=args['momentum'])
+
+    if len(args['snapshot']) > 0:
+        print('Training Resumes From \'%s\'' % args['snapshot'])
+        net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth')))
+
+    net = nn.DataParallel(net, device_ids=device_ids)
+
+    open(log_path, 'w').write(str(args) + '\n\n')
+    train(net, optimizer)
+    writer.close()
+
+
+def train(net, optimizer):
+    curr_iter = 1
+
+    for epoch in range(args['last_epoch'] + 1, args['last_epoch'] + 1 + args['epoch_num']):
+        loss_record, loss_f_4_record, loss_f_3_record, loss_f_2_record, loss_f_1_record, loss_f_record, \
+                     loss_b_4_record, loss_b_3_record, loss_b_2_record, loss_b_1_record, loss_b_record, \
+                     loss_o_record = AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), \
+                                     AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter(), AvgMeter()
+
+        train_iterator = tqdm(train_loader, total=len(train_loader))
+        for data in train_iterator:
+            if args['poly_train']:
+                base_lr = args['lr'] * (1 - float(curr_iter) / (args['epoch_num'] * len(train_loader))) ** args[
+                    'lr_decay']
+                optimizer.param_groups[0]['lr'] = 2 * base_lr
+                optimizer.param_groups[1]['lr'] = 1 * base_lr
+
+            inputs, labels = data
+            batch_size = inputs.size(0)
+            inputs = Variable(inputs).cuda(device_ids[0])
+            labels = Variable(labels).cuda(device_ids[0])
+
+            optimizer.zero_grad()
+
+            predict_f_4, predict_f_3, predict_f_2, predict_f_1, predict_f, \
+            predict_b_4, predict_b_3, predict_b_2, predict_b_1, predict_b, predict_o = net(inputs)
+
+            loss_f_4 = L.lovasz_hinge(predict_f_4, labels)
+            loss_f_3 = L.lovasz_hinge(predict_f_3, labels)
+            loss_f_2 = L.lovasz_hinge(predict_f_2, labels)
+            loss_f_1 = L.lovasz_hinge(predict_f_1, labels)
+            loss_f = L.lovasz_hinge(predict_f, labels)
+            loss_b_4 = L.lovasz_hinge(predict_b_4, 1 - labels)
+            loss_b_3 = L.lovasz_hinge(predict_b_3, 1 - labels)
+            loss_b_2 = L.lovasz_hinge(predict_b_2, 1 - labels)
+            loss_b_1 = L.lovasz_hinge(predict_b_1, 1 - labels)
+            loss_b = L.lovasz_hinge(predict_b, 1 - labels)
+            loss_o = L.lovasz_hinge(predict_o, labels)
+
+            loss = loss_f_4 + loss_f_3 + loss_f_2 + loss_f_1 + loss_f + \
+                   loss_b_4 + loss_b_3 + loss_b_2 + loss_b_1 + loss_b + loss_o
+
+            loss.backward()
+
+            optimizer.step()
+
+            loss_record.update(loss.data, batch_size)
+            loss_f_4_record.update(loss_f_4.data, batch_size)
+            loss_f_3_record.update(loss_f_3.data, batch_size)
+            loss_f_2_record.update(loss_f_2.data, batch_size)
+            loss_f_1_record.update(loss_f_1.data, batch_size)
+            loss_f_record.update(loss_f.data, batch_size)
+            loss_b_4_record.update(loss_b_4.data, batch_size)
+            loss_b_3_record.update(loss_b_3.data, batch_size)
+            loss_b_2_record.update(loss_b_2.data, batch_size)
+            loss_b_1_record.update(loss_b_1.data, batch_size)
+            loss_b_record.update(loss_b.data, batch_size)
+            loss_o_record.update(loss_o.data, batch_size)
+
+            if curr_iter % 50 == 0:
+                writer.add_scalar('loss', loss, curr_iter)
+                writer.add_scalar('loss_f_4', loss_f_4, curr_iter)
+                writer.add_scalar('loss_f_3', loss_f_3, curr_iter)
+                writer.add_scalar('loss_f_2', loss_f_2, curr_iter)
+                writer.add_scalar('loss_f_1', loss_f_1, curr_iter)
+                writer.add_scalar('loss_f', loss_f, curr_iter)
+                writer.add_scalar('loss_b_4', loss_b_4, curr_iter)
+                writer.add_scalar('loss_b_3', loss_b_3, curr_iter)
+                writer.add_scalar('loss_b_2', loss_b_2, curr_iter)
+                writer.add_scalar('loss_b_1', loss_b_1, curr_iter)
+                writer.add_scalar('loss_b', loss_b, curr_iter)
+                writer.add_scalar('loss_o', loss_o, curr_iter)
+
+            log = '[%3d], [%5d], [%.5f], [%.5f], [f4:%.5f], [f3:%.5f],[f2:%.5f],[f1:%.5f],[f:%.5f], ' \
+                  '[b4:%.5f], [b3:%.5f],[b2:%.5f],[b1:%.5f],[b:%.5f], [o:%.5f]' % \
+                  (epoch, curr_iter, base_lr, loss_record.avg,
+                   loss_f_4_record.avg, loss_f_3_record.avg, loss_f_2_record.avg, loss_f_1_record.avg, loss_f_record.avg,
+                   loss_b_4_record.avg, loss_b_3_record.avg, loss_b_2_record.avg, loss_b_1_record.avg, loss_b_record.avg,
+                   loss_o_record.avg)
+            train_iterator.set_description(log)
+            open(log_path, 'a').write(log + '\n')
+
+            curr_iter += 1
+
+        if epoch in args['save_point']:
+            net.cpu()
+            torch.save(net.module.state_dict(), os.path.join(ckpt_path, exp_name, '%d.pth' % epoch))
+            net.cuda(device_ids[0])
+
+        if epoch >= args['epoch_num']:
+            net.cpu()
+            torch.save(net.module.state_dict(), os.path.join(ckpt_path, exp_name, '%d.pth' % epoch))
+            print("Optimization Have Done!")
+            return
+
+
+if __name__ == '__main__':
+    main()

bb285afafdbc8aa90ba0a69b23c9d7edc0b2e876
Mhaiyang <845879017@qq.com>
2019-03-01 23:10:07 +0800

    Taylor iccd 2018.3.1

--- train_mhy.py
+++ train_mhy.py
@@ -26,16 +26,16 @@ from config import msd_training_root
 from config import backbone_path
 from dataset import ImageFolder
 from misc import AvgMeter, check_mkdir
-from model.mhy2_bc import MHY2_BC
+from model.mhy3 import MHY3
 
 import loss as L
 
 cudnn.benchmark = True
 
-device_ids = [9]
+device_ids = [6]
 
 ckpt_path = './ckpt'
-exp_name = 'MHY2_msd9_bc'
+exp_name = 'MHY3_msd9'
 
 args = {
     'epoch_num': 140,
@@ -84,7 +84,7 @@ def main():
     print(args)
     print(exp_name)
 
-    net = MHY2_BC(backbone_path).cuda(device_ids[0]).train()
+    net = MHY3(backbone_path).cuda(device_ids[0]).train()
     if args['add_graph']:
         writer.add_graph(net, input_to_model=torch.rand(
             args['train_batch_size'], 3, args['scale'], args['scale']).cuda(device_ids[0]))

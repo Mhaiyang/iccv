7d33bbbd6aa9da6c2c84c0d8375b5d27400662b8
Mhaiyang <845879017@qq.com>
2019-03-06 20:41:03 +0800

    Taylor iccd 2018.3.6

--- train_mhy.py
+++ train_mhy.py
@@ -37,16 +37,33 @@ device_ids = [5]
 ckpt_path = './ckpt'
 exp_name = 'OUR2_SBU'
 
+# args = {
+#     'epoch_num': 140,
+#     'train_batch_size': 10,
+#     'last_epoch': 0,
+#     'lr': 1e-3,
+#     'lr_decay': 0.9,
+#     'weight_decay': 5e-4,
+#     'momentum': 0.9,
+#     'snapshot': '',
+#     'scale': 384,
+#     'save_point': [80, 100, 120, 140],
+#     'add_graph': True,
+#     'poly_train': True,
+#     'optimizer': 'SGD'
+# }
+
+# shadow
 args = {
     'epoch_num': 140,
-    'train_batch_size': 10,
+    'train_batch_size': 8,
     'last_epoch': 0,
-    'lr': 1e-3,
+    'lr': 1e-2,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.9,
     'snapshot': '',
-    'scale': 384,
+    'scale': 416,
     'save_point': [80, 100, 120, 140],
     'add_graph': True,
     'poly_train': True,

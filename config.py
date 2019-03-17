afb2ca31bcb86f02f57d73cfb71b4293e4538482
Mhaiyang <845879017@qq.com>
2019-03-17 23:02:08 +0800

    Taylor iccd 2018.3.17

--- config.py
+++ config.py
@@ -18,16 +18,16 @@
 # msd_testing_root = "/root/data/SBU-shadow/test"
 # backbone_path = '/root/backbone/resnext/resnext_101_32x4d.pth'
 
-msd_training_root = "/root/data/MSRA10K/train"
-msd_testing_root = "/root/data/MSRA10K/DUT-O"
-backbone_path = '/root/backbone/resnext/resnext_101_32x4d.pth'
+# msd_training_root = "/root/data/MSRA10K/train"
+# msd_testing_root = "/root/data/MSRA10K/DUT-O"
+# backbone_path = '/root/backbone/resnext/resnext_101_32x4d.pth'
 
-# msd_training_root = "/home/iccd/data/MSRA10K/train"
-# msd_testing_root = "/home/iccd/data/MSRA10K/ECSSD"
+msd_training_root = "/home/iccd/data/MSRA10K/train"
+msd_testing_root = "/home/iccd/data/MSRA10K/ECSSD"
 # msd_testing_root = "/home/iccd/data/MSRA10K/PASCAL-S"
 # msd_testing_root = "/home/iccd/data/MSRA10K/HKU-IS"
 # msd_testing_root = "/home/iccd/data/MSRA10K/DUT-OMRON"
-# backbone_path = '/home/iccd/iccv/backbone/resnext/resnext_101_32x4d.pth'
+backbone_path = '/home/iccd/iccv/backbone/resnext/resnext_101_32x4d.pth'
 
 # msd_training_root = "/home/iccd/data/SBU-shadow/train"
 # msd_testing_root = "/home/iccd/data/SBU-shadow/test"

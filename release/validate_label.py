1401c260324835dadc5e817521ae45a76a8d5bbb
Mhaiyang <845879017@qq.com>
2019-09-09 17:12:41 +0800

    Taylor iccd 2019.09.09

---
+++ release/validate_label.py
@@ -0,0 +1,36 @@
+"""
+ @Time    : 9/9/19 17:00
+ @Author  : TaylorMei
+ @Email   : mhy845879017@gmail.com
+ 
+ @Project : iccv
+ @File    : validate_label.py
+ @Function:
+ 
+"""
+import os
+import numpy as np
+import skimage.io
+
+image_path = '/media/iccd/disk/release/MSD/train/image'
+mask_path = '/media/iccd/disk/release/MSD/train/mask'
+output_path = '/home/iccd/iccv/release/train'
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+color= [0, 1, 0]
+
+imglist = os.listdir(image_path)
+for i, imgname in enumerate(imglist):
+    print(i, imgname)
+    image = skimage.io.imread(os.path.join(image_path, imgname))
+    mask = skimage.io.imread(os.path.join(mask_path, imgname[:-4] + '.png'))
+
+    output = np.zeros_like(image)
+
+    for j in range(image.shape[2]):
+        output[:, :, j] = np.where(mask != 0, image[:, :, j] * 0.4 + color[j] * 255 *0.6, image[:, :, j])
+
+
+    skimage.io.imsave(os.path.join(output_path, imgname), output)

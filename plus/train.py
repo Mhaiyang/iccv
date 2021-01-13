b281f8687de1fb2e0f77e419e2245870396a674e
mhaiyang <mhy666@mail.dlut.edu.cn>
2021-01-13 19:45:15 +0800

    2080Ti 2021.01.13

--- plus/train.py
+++ plus/train.py
@@ -37,10 +37,11 @@ import loss as L
 
 cudnn.benchmark = True
 
-device_ids = [0]
+device_ids = [1]
 
-ckpt_path = './ckpt'
-exp_name = 'MirrorNet_Plus_3'
+# ckpt_path = './ckpt'
+ckpt_path = '/media/iccd/disk1/tip_mirror_ckpt'
+exp_name = 'MirrorNet_Plus_4'
 
 args = {
     'epoch_num': 200,

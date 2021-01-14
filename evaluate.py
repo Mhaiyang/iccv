92f2dfd70cf55b7df2db4a9b541e64753afd2e65
meihaiyang <mhy666@mail.dlut.edu.cn>
2021-01-14 21:00:32 +0800

    3090 2021.01.13

--- evaluate.py
+++ evaluate.py
@@ -28,7 +28,7 @@ MASK_DIR = os.path.join(msd_testing_root, "mask")
 # PREDICT_DIR = os.path.join(ROOT_DIR, ckpt_path, exp_name, '%s_%s' % (exp_name, args['snapshot']))
 # PREDICT_DIR = "/home/iccd/iccv/ckpt/TAYLOR5/TAYLOR5_160_crf/f1"
 # PREDICT_DIR = "/home/iccd/iccv/ckpt/TAYLOR5_NOC/TAYLOR5_NOC_140/f1"
-PREDICT_DIR = "./plus/ckpt/MirrorNet_Plus_2/MirrorNet_Plus_2_epoch_195_ber_7.60_crf"
+PREDICT_DIR = "./plus/ckpt/MirrorNet_Plus_3/MirrorNet_Plus_3_epoch_190_ber_5.77_crf"
 # PREDICT_DIR = "/home/iccd/R3Net/ckpt/R3Net/(R3Net) HKU-IS_6000"
 
 if args['type'] != 0:

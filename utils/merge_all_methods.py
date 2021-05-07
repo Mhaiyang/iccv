4d1b1133082035f6ee8cf69f135f0d96f03c44c5
meihaiyang <mhy666@mail.dlut.edu.cn>
2021-05-07 16:34:29 +0800

    3090 2021.05.07

--- utils/merge_all_methods.py
+++ utils/merge_all_methods.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-root_dir = '/home/iccd/iccv/visual/methods'
-output_dir = '/home/iccd/iccv/visual/all'
+root_dir = '/home/iccd/iccv/plus/ablation_methods'
+output_dir = '/home/iccd/iccv/plus/ablation_all'
 methods = os.listdir(root_dir)
 for method in methods:
     images = os.listdir(os.path.join(root_dir, method))
     for image in images:
         src = os.path.join(root_dir, method, image)
         dst = os.path.join(output_dir, image[:-4] + '_' + method + image[-4:])
-        shutil.move(src, dst)
+        shutil.copy(src, dst)
 
 print('OK!')
 
